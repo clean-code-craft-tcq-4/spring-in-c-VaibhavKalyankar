fad93ae28343b989d8167dd0e1ef743bae1a7f9c
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-05-14 23:35:52 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -15,5 +15,10 @@ void ledAlerter()
 {
     
 }
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    
+}
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
