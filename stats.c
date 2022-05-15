ad08cf14e52adf5f1a0f230dfb47187f7178aa24
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-05-15 20:09:32 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -4,22 +4,33 @@
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     int total=0;
-    s.min = numberset[0];
-    s.max = numberset[0];
-       for (int i=0;i<setlength;i++)
-       {
-           printf("numberset[i]=%f",numberset[i]);
-           total = total + numberset[i];
-            if(numberset[i] > s.max)
-                s.max = numberset[i];
-           
-            if(numberset[i] < s.min)
-                s.min = numberset[i];
-       }
     
-       
-    
-    s.average = (total/setlength);
+    if(numberset != NULL)
+    {
+        s.min = numberset[0];
+        s.max = numberset[0];
+           for (int i=0;i<setlength;i++)
+           {
+               printf("numberset[i]=%f",numberset[i]);
+               total = total + numberset[i];
+                if(numberset[i] > s.max)
+                    s.max = numberset[i];
+
+                if(numberset[i] < s.min)
+                    s.min = numberset[i];
+           }
+
+
+
+        s.average = (total/setlength);
+    }
+    else
+    {
+         computedStats.average = NAN;
+        computedStats.max = NAN;
+        computedStats.min = NAN;
+        
+    }
     
     //s.max = numberset[2];
     return s;
