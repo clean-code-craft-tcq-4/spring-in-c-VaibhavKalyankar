0df7e8477b89b2db6d844db0a204e6c44eaa9854
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-05-15 20:21:13 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -13,10 +13,10 @@ TEST_CASE("reports average, minimum and maximum") {
     
     
     float epsilon = 0.001;
-    printf("avg=%f,max=%f,min=%f",computedStats.average,computedStats.max,computedStats.min);
-    REQUIRE((computedStats.average - 4.525) < epsilon);
-    REQUIRE((computedStats.max - 8.9) < epsilon);
-    REQUIRE((computedStats.min - 1.5) < epsilon);
+  //  printf("avg=%f,max=%f,min=%f",computedStats.average,computedStats.max,computedStats.min);
+    REQUIRE(abs(computedStats.average - 4.525) < epsilon);
+    REQUIRE(abs(computedStats.max - 8.9) < epsilon);
+    REQUIRE(abs(computedStats.min - 1.5) < epsilon);
     
 }
 
